8487da975ae186f3365d7ad2b2f2d3b274748575
amoghmc <amoghmc@github.com>
2023-05-06 03:42:41 +0530

    refactor 10

--- 10_sr_parser.c
+++ 10_sr_parser.c
@@ -1,77 +1,79 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <curses.h>
 #include <string.h>
-int k = 0, z = 0, i = 0, j = 0, c = 0;
-char a[16], ac[20], stk[15], act[10];
+int k = 0, stk_index = 0, i = 0, j = 0, c = 0;
+char inp_str[16], ac[20], stk[15], act[10];
 void check();
-void main()
+
+int main()
 {
     puts("GRAMMAR is E->E+E \n E->E*E \n E->(E) \n E->id");
     puts("enter input string ");
-    gets(a);
-    c = strlen(a);
+    gets(inp_str);
+    c = strlen(inp_str);
     strcpy(act, "SHIFT->");
     puts("stack \t input \t action");
     for (k = 0, i = 0; j < c; k++, i++, j++)
     {
-        if (a[j] == 'i' && a[j + 1] == 'd')
+        if (inp_str[j] == 'i' && inp_str[j + 1] == 'd')
         {
-            stk[i] = a[j];
-            stk[i + 1] = a[j + 1];
+            stk[i] = inp_str[j];
+            stk[i + 1] = inp_str[j + 1];
             stk[i + 2] = '\0';
-            a[j] = ' ';
-            a[j + 1] = ' ';
-            printf("\n$%s\t%s$\t%sid", stk, a, act);
+            inp_str[j] = ' ';
+            inp_str[j + 1] = ' ';
+            printf("\n$%s\t%s$\t%sid", stk, inp_str, act);
             check();
         }
         else
         {
-            stk[i] = a[j];
+            stk[i] = inp_str[j];
             stk[i + 1] = '\0';
-            a[j] = ' ';
-            printf("\n$%s\t%s$\t%ssymbols", stk, a, act);
+            inp_str[j] = ' ';
+            printf("\n$%s\t%s$\t%ssymbols", stk, inp_str, act);
             check();
         }
     }
     getch();
 }
+
 void check()
 {
-
     strcpy(ac, "REDUCE TO E");
-    for (z = 0; z < c; z++)
-        if (stk[z] == 'i' && stk[z + 1] == 'd')
+    for (stk_index = 0; stk_index < c; stk_index++)
+        if (stk[stk_index] == 'i' && stk[stk_index + 1] == 'd')
         {
-            stk[z] = 'E';
-            stk[z + 1] = '\0';
-            printf("\n$%s\t%s$\t%s", stk, a, ac);
+            stk[stk_index] = 'E';
+            stk[stk_index + 1] = '\0';
+            printf("\n$%s\t%s$\t%s", stk, inp_str, ac);
             j++;
         }
-    for (z = 0; z < c; z++)
-        if (stk[z] == 'E' && stk[z + 1] == '+' && stk[z + 2] == 'E')
+    for (stk_index = 0; stk_index < c; stk_index++)
+        if (stk[stk_index] == 'E' && stk[stk_index + 1] == '+' && stk[stk_index + 2] == 'E')
         {
-            stk[z] = 'E';
-            stk[z + 1] = '\0';
-            stk[z + 2] = '\0';
-            printf("\n$%s\t%s$\t%s", stk, a, ac);
+            stk[stk_index] = 'E';
+            stk[stk_index + 1] = '\0';
+            stk[stk_index + 2] = '\0';
+            printf("\n$%s\t%s$\t%s", stk, inp_str, ac);
             i = i - 2;
         }
-    for (z = 0; z < c; z++)
-        if (stk[z] == 'E' && stk[z + 1] == '*' && stk[z + 2] == 'E')
+    for (stk_index = 0; stk_index < c; stk_index++)
+        if (stk[stk_index] == 'E' && stk[stk_index + 1] == '*' && stk[stk_index + 2] == 'E')
         {
-            stk[z] = 'E';
-            stk[z + 1] = '\0';
-            stk[z + 1] = '\0';
-            printf("\n$%s\t%s$\t%s", stk, a, ac);
+            stk[stk_index] = 'E';
+            stk[stk_index + 1] = '\0';
+            stk[stk_index + 1] = '\0';
+            printf("\n$%s\t%s$\t%s", stk, inp_str, ac);
             i = i - 2;
         }
-    for (z = 0; z < c; z++)
-        if (stk[z] == '(' && stk[z + 1] == 'E' && stk[z + 2] == ')')
+    for (stk_index = 0; stk_index < c; stk_index++)
+        if (stk[stk_index] == '(' && stk[stk_index + 1] == 'E' && stk[stk_index + 2] == ')')
         {
-            stk[z] = 'E';
-            stk[z + 1] = '\0';
-            stk[z + 1] = '\0';
-            printf("\n$%s\t%s$\t%s", stk, a, ac);
+            stk[stk_index] = 'E';
+            stk[stk_index + 1] = '\0';
+            stk[stk_index + 1] = '\0';
+            printf("\n$%s\t%s$\t%s", stk, inp_str, ac);
             i = i - 2;
         }
 }
